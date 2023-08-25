c74e35aa28d6005da7fd93709a2ac3267d555c94
JuanM-222 <juanmontoya82522@gmail.com>
2023-08-25 14:37:23 -0500

    implementación de la pagina principal y los juegos
    Se crea la pagina principal con el nombre de index.html donde contiene una breve descripción del los juegos , también los botones para acceder a la pagina web , al igual que el desarrollo de cada uno de los juegos

---
+++ logica de js/index.js
@@ -0,0 +1,35 @@
+// Obtener el botón por su ID
+let botonAho = document.getElementById('ahorcado');
+
+// Añadir un evento 'click' al botón
+botonAho.addEventListener('click', function() {
+    // Redirigir al usuario a la página deseada
+    window.location.href = "ahorcado.html";
+});
+
+// Obtener el botón por su ID
+let botonTri = document.getElementById('triki');
+
+// Añadir un evento 'click' al botón
+botonTri.addEventListener('click', function() {
+    // Redirigir al usuario a la página deseada
+    window.location.href = "triki.html";
+});
+
+// Obtener el botón por su ID
+let botonSna = document.getElementById('snake');
+
+// Añadir un evento 'click' al botón
+botonSna.addEventListener('click', function() {
+    // Redirigir al usuario a la página deseada
+    window.location.href = 'snake.html';
+});
+
+// Obtener el botón por su ID
+let botonMem = document.getElementById('memoria');
+
+// Añadir un evento 'click' al botón
+botonMem.addEventListener('click', function() {
+    // Redirigir al usuario a la página deseada
+    window.location.href = "memoria.html";
+});
